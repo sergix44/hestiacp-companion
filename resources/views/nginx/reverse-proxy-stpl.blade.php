1e40418245ac3a91eb893c6fbb45c78be05be557
Sergio Brighenti <sergio@brighenti.me>
2023-08-19 23:28:02 +0200

    make whole thing changeable

--- resources/views/nginx/reverse-proxy-stpl.blade.php
+++ resources/views/nginx/reverse-proxy-stpl.blade.php
@@ -17,7 +17,7 @@ server {
         proxy_set_header        X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_set_header        X-Forwarded-Proto $scheme;
         proxy_redirect http:// https://;
-        proxy_pass              http://%proxy_extensions%;
+        proxy_pass              %proxy_extensions%;
     }
 
     location /error/ {
